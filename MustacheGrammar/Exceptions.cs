db5b999a0530d722d3f797eab2894b96b459d7c6
Nemo-Illusionist <illusionist.nemo@gmail.com>
2017-03-06 00:15:56 +0200

    написан Mustache

---
+++ MustacheGrammar/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MustacheGrammar
+{
+    public class KeyOrValueException : Exception
+    {
+        public KeyOrValueException(string message) : base(message) { }
+    }
+
+    public class NotKeyException : Exception
+    {
+        public NotKeyException(string message) : base(message) { }
+    }
+
+}
--- MustacheGrammar/Mustache.cs
+++ MustacheGrammar/Mustache.cs
@@ -1,24 +1,101 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace MustacheGrammar
 {
     public class Mustache
     {
-        private readonly Dictionary<string, string> Labels;
-        private readonly List<Pattern> _patterns;
+        private readonly Dictionary<string, string> _dictionary;
 
         public Mustache()
         {
-            Labels = new Dictionary<string, string>();
-            _patterns = new List<Pattern>
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string Pars(string text, string dictionary)
+        {
+            ParsDictionary(dictionary);
+            bool k = true;
+            while (k)
             {
-                 new Pattern {Regex = new Regex(@"{{\s*(\w+)\s*}}", RegexOptions.IgnoreCase), Action = null},
-                 new Pattern {Regex = new Regex(@"{{\s*(\w+ )\s*\|\s+upper\s*}}", RegexOptions.IgnoreCase), Action = null},
-                 new Pattern {Regex = new Regex(@"{{\s*(\w+)\s*\|\s+lower\s*}}", RegexOptions.IgnoreCase), Action = null},
-                 new Pattern {Regex = new Regex(@"{{\s*(\w+)\s*\|\s+title\s*}}", RegexOptions.IgnoreCase), Action = null},
-                 new Pattern {Regex = new Regex(@"{{\s*ifnot\s*(\w+):\s*(\w+)\s*}}", RegexOptions.IgnoreCase), Action = null},
-            };
+                string textClone = text;
+
+                Regex r = new Regex(@"{{\s*(\w+)\s*}}");
+                text = r.Replace(text, Repl);
+
+                r = new Regex(@"{{\s*(\w+)\s*\|\s+upper\s*}}");
+                text = r.Replace(text, ReplUpper);
+
+                r = new Regex(@"{{\s*(\w+)\s*\|\s+lower\s*}}");
+                text = r.Replace(text, ReplLower);
+
+                r = new Regex(@"{{\s*(\w+)\s*\|\s+title\s*}}");
+                text = r.Replace(text, ReplTitle);
+
+                r = new Regex(@"{{\s*ifnot\s*(\w+):\s*(\w+)\s*}}");
+                text = r.Replace(text, ReplIfnot);
+
+                k = text != textClone;
+            }
+            return text;
+        }
+
+        private void ParsDictionary(string dictionary)
+        {
+            string[] lines = dictionary.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            Regex r = new Regex(@"(\w+)\s*-\s*(\w+)");
+            foreach (var l in lines)
+            {
+                Match m = r.Match(l.Trim());
+                if (m.Success)
+                {
+                    _dictionary.Add(m.Groups[1].Value, m.Groups[2].Value);
+                }
+                else
+                {
+                    throw new KeyOrValueException("Не удалось распознать Ключь-Значение");
+                }
+            }
+        }
+
+        private void KeyCorrect(string key)
+        {
+            if (!_dictionary.ContainsKey(key))
+                throw new NotKeyException("Попытка обратится к несуществующему ключу");
+        }
+
+        private string Repl(Match mtch)
+        {
+            KeyCorrect(mtch.Groups[1].Value);
+            return _dictionary[mtch.Groups[1].Value];
+        }
+
+        private string ReplUpper(Match mtch)
+        {
+            KeyCorrect(mtch.Groups[1].Value);
+            return _dictionary[mtch.Groups[1].Value].ToUpper();
+        }
+
+        private string ReplLower(Match mtch)
+        {
+            KeyCorrect(mtch.Groups[1].Value);
+            return _dictionary[mtch.Groups[1].Value].ToLower();
+        }
+
+        private string ReplTitle(Match mtch)
+        {
+            KeyCorrect(mtch.Groups[1].Value);
+            return _dictionary[mtch.Groups[1].Value].Substring(0, 1).ToUpper()
+                + (_dictionary[mtch.Groups[1].Value].Length > 1 ?
+                    _dictionary[mtch.Groups[1].Value].Substring(1) : "");
+        }
+
+        private string ReplIfnot(Match mtch)
+        {
+            return _dictionary.ContainsKey(mtch.Groups[1].Value)
+                ? _dictionary[mtch.Groups[1].Value]
+                : mtch.Groups[2].Value;
         }
     }
 }
