924f088d879a219ef2c1381e2d201d49c965cdf8
Nemo-Illusionist <illusionist.nemo@gmail.com>
2017-03-06 11:10:17 +0200

    начата работа над ML

--- AsmGrammar/Exceptions.cs
+++ AsmGrammar/Exceptions.cs
@@ -6,27 +6,31 @@ namespace AsmGrammar
     {
         public ParserException(string message) :
             base("Строка не распознана: " + message)
-        { }
+        {
+        }
     }
 
     public class AddressException : Exception
     {
         public AddressException(int ip, string g) :
             base(string.Format("Строка {0}: Используется несуществующий адрес: {1}", ip, g))
-        { }
+        {
+        }
     }
 
     public class LabelUnavailableException : Exception
     {
         public LabelUnavailableException(int ip, string g) :
             base(string.Format("Строка {0}: Попытка перейти на несуществующую метку: {1}", ip, g))
-        { }
+        {
+        }
     }
 
     public class ParameterOutOfRangeException : Exception
     {
         public ParameterOutOfRangeException(int ip, string g) :
             base(string.Format("Строка {0}: Заданный аргумент ({1}) находится вне диапазона допустимых значений", ip, g))
-        { }
+        {
+        }
     }
 }
---
+++ MdLiteGrammar/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MdLiteGrammar
+{
+    public class KeyOrValueException : Exception
+    {
+        public KeyOrValueException(string message) : base(message) { }
+    }
+
+    public class NotKeyException : Exception
+    {
+        public NotKeyException(string message) : base(message) { }
+    }
+
+}
