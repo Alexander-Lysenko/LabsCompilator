4447ad9400a730a4b9cf8b91b6796ec51eba3b45
illusionist nemo <illusionist nemo>
2017-03-13 10:52:01 +0200

    add automat grammori

---
+++ AutomaticGrammar/RealNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace AutomaticGrammar
+{
+    public class RealNumbers
+    {
+        private readonly int[,] _table;
+        private readonly int[] _final;
+
+        public RealNumbers()
+        {
+            _table = new[,]{
+                {2,1,7,7,7},
+                {1,7,7,7,7},
+                {2,7,3,4,7},
+                {3,7,7,4,7},
+                {6,5,7,7,7},
+                {6,7,7,7,7},
+                {6,7,7,7,7},
+                {7,7,7,7,7},
+            };
+            _final = new[] { 2, 3, 6 };
+        }
+
+        private int ch(char c)
+        {
+            if (char.IsDigit(c))
+                return 0;
+            if (c == '+' || c == '-')
+                return 1;
+            if (c == '.' || c == ',')
+                return 2;
+            if (c == 'e' || c == 'E')
+                return 3;
+            return 4;
+        }
+
+        public bool lol(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentNullException();
+            int status = text.Aggregate(0, (current, c) => _table[current, ch(c)]);
+            return Array.IndexOf(_final, status) != -1;
+        }
+    }
+}
