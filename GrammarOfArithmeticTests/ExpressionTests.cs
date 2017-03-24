70fd520b3878ec6a490bb647e5611dc20373393f
illusionist <illusionist.nemo@gmail.com>
2017-03-24 15:21:10 +0200

    работа над граматикой

--- GrammarOfArithmeticTests/ExpressionTests.cs
+++ GrammarOfArithmeticTests/ExpressionTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrammarOfArithmetic;
 
 namespace GrammarOfArithmetic.Tests
 {
@@ -8,12 +9,12 @@ namespace GrammarOfArithmetic.Tests
         [TestMethod()]
         public void ParseTest1()
         {
-            Expression ex;
-            ex = new Expression("(50)");
+            ExpressionModule ex;
+            ex = new ExpressionModule("(50)");
             Assert.AreEqual(ex.Parse(), 50);
-            ex = new Expression("(+50)");
+            ex = new ExpressionModule("(+50)");
             Assert.AreEqual(ex.Parse(), 50);
-            ex = new Expression("(-50)");
+            ex = new ExpressionModule("(-50)");
             Assert.AreEqual(ex.Parse(), -50);
 
         }
@@ -21,28 +22,28 @@ namespace GrammarOfArithmetic.Tests
         [TestMethod()]
         public void ParseTest2()
         {
-            Expression ex;
-            ex = new Expression("2+3");
+            ExpressionModule ex;
+            ex = new ExpressionModule("2+3");
             Assert.AreEqual(ex.Parse(), 5);
-            ex = new Expression("3*3+3");
+            ex = new ExpressionModule("3*3+3");
             Assert.AreEqual(ex.Parse(), 12);
-            ex = new Expression("(2*2)+2");
+            ex = new ExpressionModule("(2*2)+2");
             Assert.AreEqual(ex.Parse(), 6);
-            ex = new Expression("(2+2)*2");
+            ex = new ExpressionModule("(2+2)*2");
             Assert.AreEqual(ex.Parse(), 8);
         }
 
         [TestMethod()]
         public void ParseTest3()
         {
-            Expression ex;
-            ex = new Expression("2+-3");
+            ExpressionModule ex;
+            ex = new ExpressionModule("2+-3");
             Assert.AreEqual(ex.Parse(), -1);
-            ex = new Expression("3*3--3");
+            ex = new ExpressionModule("3*3--3");
             Assert.AreEqual(ex.Parse(), 12);
-            ex = new Expression("(2*-2)+2");
+            ex = new ExpressionModule("(2*-2)+2");
             Assert.AreEqual(ex.Parse(), -2);
-            ex = new Expression("-(2+2)*2");
+            ex = new ExpressionModule("-(2+2)*2");
             Assert.AreEqual(ex.Parse(), -8);
         }
 
